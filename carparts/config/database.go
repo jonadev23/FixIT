1488e2e3f3713c116157b26fa546302469c4390f
jonadev23 <jonathandevops34@gmail.com>
2025-05-22 08:38:27 +0300

    Changed production url

--- carparts/config/database.go
+++ carparts/config/database.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 // ConnectDB initializes the database connection
 func ConnectDB() {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",  //disable or require
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
