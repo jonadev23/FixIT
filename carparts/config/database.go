48d9221024cc34fb9890329c04c9955b342d478e
jonadev23 <jonathandevops34@gmail.com>
2025-05-22 09:02:43 +0300

    production url again

--- carparts/config/database.go
+++ carparts/config/database.go
@@ -14,14 +14,22 @@ var DB *gorm.DB
 
 // ConnectDB initializes the database connection
 func ConnectDB() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",  //disable or require
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+var dsn string
+
+	if os.Getenv("ENV") == "production" {
+		// Use full DATABASE_URL from Render or other cloud host
+		dsn = os.Getenv("DATABASE_URL")
+	} else {
+		// Local development
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", // disable for local
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
