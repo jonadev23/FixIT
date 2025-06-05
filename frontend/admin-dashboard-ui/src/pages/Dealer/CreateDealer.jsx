155984b9a39a320fcf02f14be436738e44a03a15
jonadev23 <jonathandevops34@gmail.com>
2025-06-05 11:34:16 +0300

    New styles and no dropping tables

--- frontend/admin-dashboard-ui/src/pages/Dealer/CreateDealer.jsx
+++ frontend/admin-dashboard-ui/src/pages/Dealer/CreateDealer.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 import { backendUrl } from "../../utils/auth";
+import Lottie from "lottie-react";
+import spinnerJson from "../../assets/lotties/spinner.json";
 
 const CreateDealer = () => {
   const [dealer, setDealer] = useState({
@@ -8,6 +10,7 @@ const CreateDealer = () => {
     email: "",
     contact: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const [message, setMessage] = useState("");
 
@@ -16,75 +19,95 @@ const CreateDealer = () => {
   };
 
   const handleSubmit = async (e) => {
+    setLoading(!loading);
     e.preventDefault();
     try {
-      const response = await axios.post(
-        `${backendUrl}/api/dealers`,
-        dealer
-      );
+      const response = await axios.post(`${backendUrl}/api/dealers`, dealer);
       setMessage("Dealer created successfully!");
+      setLoading(false);
       //   setDealer({ first_name: "",last_name:"", email: "", contact: "" });
     } catch (error) {
       console.error("Error creating dealer:", error);
       setMessage("Failed to create dealer.");
+      setLoading(false);
     }
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-5 bg-white shadow-md rounded">
+    <div className="max-w-md mx-auto mt-10 p-5 bg-white shadow-sm rounded">
       <h2 className="text-2xl font-bold mb-5">Create Dealer</h2>
+
       {message && <p className="text-green-600">{message}</p>}
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label className="block font-medium">First Name</label>
+          <label className="block font-medium text-sm">First Name</label>
           <input
             type="text"
             name="first_name"
             value={dealer.first_name}
             onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded"
+            className="w-full p-2 border text-sm border-gray-300 rounded"
             required
           />
         </div>
         <div className="mb-3">
-          <label className="block font-medium">Last Name</label>
+          <label className="block font-medium text-sm">Last Name</label>
           <input
             type="text"
             name="last_name"
             value={dealer.last_name}
             onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded"
+            className="w-full p-2 border text-sm border-gray-300 rounded"
             required
           />
         </div>
         <div className="mb-3">
-          <label className="block font-medium">Email</label>
+          <label className="block font-medium text-sm">Email</label>
           <input
             type="email"
             name="email"
             value={dealer.email}
             onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded"
+            className="w-full p-2 border text-sm border-gray-300 rounded"
             required
           />
         </div>
         <div className="mb-3">
-          <label className="block font-medium">Contact</label>
+          <label className="block font-medium text-sm">Contact</label>
           <input
             type="text"
             name="number"
             value={dealer.number}
             onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded"
+            className="w-full p-2 border text-sm border-gray-300 rounded"
             required
           />
         </div>
-        <button
-          type="submit"
-          className="bg-blue-600 text-white p-2 rounded w-full"
-        >
-          Create Dealer
-        </button>
+        <div className="cursor-not-allowed">
+          {" "}
+          <button
+            type="submit"
+            className={
+              loading
+                ? "pointer-events-none rounded bg-black w-full flex justify-center text-center "
+                : `cursor-pointer bg-black hover:bg-black/80 transition-colors duration-200 text-white p-2 rounded w-full`
+            }
+          >
+            {loading ? (
+              <div className="">
+                {" "}
+                <Lottie
+                  animationData={spinnerJson}
+                  loop
+                  autoplay
+                  style={{ width: 40, height: 40 }}
+                />
+              </div>
+            ) : (
+              <>Create Dealer</>
+            )}{" "}
+          </button>
+        </div>
       </form>
     </div>
   );
